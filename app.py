65d2c3a70a9934e317147ac9764f8dca47c5ede7
EC2 Default User <ec2-user@ip-172-31-37-145.ap-south-1.compute.internal>
2026-04-01 20:42:33 +0000

    hhehuhfue

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "DevOps Project Running using Jenkins"
+    return "DevOps Project Running using JenkinsCi/Cd"
 
 @app.route("/health")
 def health():
