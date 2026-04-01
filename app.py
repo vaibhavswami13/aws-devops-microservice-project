8fb9989c5439561b5d899e77d120676b918094d8
EC2 Default User <ec2-user@ip-172-31-37-145.ap-south-1.compute.internal>
2026-04-01 19:09:53 +0000

    flask-project

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "DevOps Project Running"
+
+@app.route("/health")
+def health():
+    return {
+        "status": "healthy",
+        "hostname": socket.gethostname()
+    }
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
